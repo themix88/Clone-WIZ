34d4a22c8ce9bd2a6bcfe253a8050702e3b4ff9d
themix88 <87669308+themix88@users.noreply.github.com>
2026-06-05 02:55:45 +0200

    updates

--- Rclone-WIZ/rclone-wiz.py
+++ Rclone-WIZ/rclone-wiz.py
@@ -186,7 +186,7 @@ class RcloneKdeApp(QMainWindow):
             "A tool to configure, script, and mount cloud drives using rclone."
             "\n\nCreated by Miran Kljun\n2026\ngithub.com/themix88/" \
             "\n\n"
-            "\n\nSpecial thanks goes to rclone developer & community for their amazing work! Without them, this project wouldn't be possible.")
+            "\n\nSpecial thanks goes to rclone developers & community for their amazing work!\nhttps://rclone.org/authors/\nhttps://rclone.org/")
 
     def _show_tutorial(self):
         """Displays a formatted HTML dialog guiding the user through GDrive setup."""
